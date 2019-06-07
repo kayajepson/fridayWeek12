b9900241b55a61a45944a725e44846eeda3866ed
kaya <jepsonkaya@gmail.com>
2019-06-07 13:08:51 -0700

    clean up old code

--- src/functions.js
+++ src/functions.js
@@ -1,41 +1,9 @@
 export class DoctorQuery {
-  // getDoctors() {
-  //    return new Promise(function(resolve, reject) {
-  //      let request = new XMLHttpRequest();
-  //      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=2&limit=100&user_key=${process.env.API_KEY}`;
-  //      request.onload = function() {
-  //        if (this.status === 200) {
-  //          resolve(request.response);
-  //        } else {
-  //          reject(Error(request.statusText));
-  //        }
-  //      }
-  //      request.open("GET", url, true);
-  //      request.send();
-  //    });
-  //  }
-   // byAilment(input) {
-   //    return new Promise(function(resolve, reject) {
-   //      let userInput = input;
-   //      let request = new XMLHttpRequest();
-   //      let url = `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${userInput}&location=or-portland&skip=2&limit=100&user_key=${process.env.API_KEY}`;
-   //      request.onload = function() {
-   //        if (this.status === 200) {
-   //          resolve(request.response);
-   //        } else {
-   //          reject(Error(request.statusText));
-   //        }
-   //      }
-   //      request.open("GET", url, true);
-   //      request.send();
-   //    });
-   //  }
-
     byConditionQuery(input) {
        return new Promise(function(resolve, reject) {
          let userInput = input;
          let request = new XMLHttpRequest();
-         let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userInput}&location=or-portland&skip=2&limit=100&user_key=${process.env.API_KEY}`;
+         let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userInput}&location=or-portland&skip=2&limit=100&user_key=${process.env.apiKey}`;
          request.onload = function() {
            if (this.status === 200) {
              resolve(request.response);
@@ -52,7 +20,7 @@ export class DoctorQuery {
        return new Promise(function(resolve, reject) {
          let userInput = input;
          let request = new XMLHttpRequest();
-         let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${userInput}?&location=or-portland&skip=2&limit=100&user_key=${process.env.API_KEY}`;
+         let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${userInput}?&location=or-portland&skip=2&limit=100&user_key=${process.env.apiKey}`;
          request.onload = function() {
            if (this.status === 200) {
              resolve(request.response);
