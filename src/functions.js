81777e51d54ca60674e39497e7604f2307e29741
kaya <jepsonkaya@gmail.com>
2019-06-07 10:17:30 -0700

    add function to display doctors based on search term. still working on getting term to display correctly next to doctor name

--- src/functions.js
+++ src/functions.js
@@ -2,7 +2,7 @@ export class DoctorQuery {
   getDoctors() {
      return new Promise(function(resolve, reject) {
        let request = new XMLHttpRequest();
-       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}`;
+       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=2&limit=100&user_key=${process.env.API_KEY}`;
        request.onload = function() {
          if (this.status === 200) {
            resolve(request.response);
@@ -14,4 +14,21 @@ export class DoctorQuery {
        request.send();
      });
    }
+   byAilment(input) {
+      return new Promise(function(resolve, reject) {
+        let userInput = input;
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${userInput}&location=or-portland&skip=2&limit=100&user_key=${process.env.API_KEY}`;
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        }
+        request.open("GET", url, true);
+        request.send();
+      });
+    }
+
 }
--- src/main.js
+++ src/main.js
@@ -2,19 +2,34 @@ import { DoctorQuery } from "./functions.js";
 import $ from 'jquery';
 
 $(document).ready(function() {
+
   $('#doctorList').click(function() {
     let doctorQuery = new DoctorQuery();  // create instance of DoctorQuery class
     let promise = doctorQuery.getDoctors();  // call the instance method
-
     promise.then(function(response) {
       let body = JSON.parse(response);
         (body.data).forEach(doctor => {
           $('.showDoctors').append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}</li>`);
         })
-
-
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
+
+  $('#byAilmentShow').click(function() {
+    let input = $('#ailment').val();
+    $('#ailment').val("");
+    let doctorQuery = new DoctorQuery();  // create instance of DoctorQuery class
+    let promise = doctorQuery.byAilment(input);  // call the instance method
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      console.log(body);
+        (body.data).forEach(doctor => {
+          $('.showDoctorsAilment').append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} ${doctor.profile.specialty_uid}</li>`);
+        })
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+
 });
