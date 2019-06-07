a6b6cac40c5185c01f297dca9443375ec59b7fe4
kaya <jepsonkaya@gmail.com>
2019-06-07 12:26:51 -0700

    remove unneeded methods in DoctorQuery

--- src/functions.js
+++ src/functions.js
@@ -1,19 +1,19 @@
 export class DoctorQuery {
-  getDoctors() {
-     return new Promise(function(resolve, reject) {
-       let request = new XMLHttpRequest();
-       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=2&limit=100&user_key=${process.env.API_KEY}`;
-       request.onload = function() {
-         if (this.status === 200) {
-           resolve(request.response);
-         } else {
-           reject(Error(request.statusText));
-         }
-       }
-       request.open("GET", url, true);
-       request.send();
-     });
-   }
+  // getDoctors() {
+  //    return new Promise(function(resolve, reject) {
+  //      let request = new XMLHttpRequest();
+  //      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=2&limit=100&user_key=${process.env.API_KEY}`;
+  //      request.onload = function() {
+  //        if (this.status === 200) {
+  //          resolve(request.response);
+  //        } else {
+  //          reject(Error(request.statusText));
+  //        }
+  //      }
+  //      request.open("GET", url, true);
+  //      request.send();
+  //    });
+  //  }
    // byAilment(input) {
    //    return new Promise(function(resolve, reject) {
    //      let userInput = input;
--- src/main.js
+++ src/main.js
@@ -2,44 +2,6 @@ import { DoctorQuery } from "./functions.js";
 import $ from 'jquery';
 
 $(document).ready(function() {
-
-  $('#doctorList').click(function() {
-    let doctorQuery = new DoctorQuery();
-    let promise = doctorQuery.getDoctors();
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-        (body.data).forEach(doctor => {
-          $('.showDoctors').append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}</li>`);
-        })
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
-  });
-
-  // $('#byAilmentShow').click(function() {
-  //   let input = $('#ailment').val();
-  //   $('#ailment').val("");
-  //   let doctorQuery = new DoctorQuery();
-  //   let promise = doctorQuery.byAilment(input);
-  //   promise.then(function(response) {
-  //     let body = JSON.parse(response);
-  //     console.log("body", body);
-  //     console.log("body data" , body.data);
-  //     if((body.data).length > 0) {
-  //       $('#headAilment').html(`<h4>Here are some doctors that specialize in ${input}</h4>`);
-  //       let data = body.data;
-  //       console.log("data", data);
-  //       for (let i=0; i < data.length; i++){
-  //         $('.showDoctorsAilment').append(`<li>${data[i].profile.first_name} ${data[i].profile.last_name}<ul><li>${data[i].practices[0].visit_address.street} ${data[i].practices[0].visit_address.city} ${data[i].practices[0].visit_address.state}</li> <li>${data[i].practices[0].phones[0].number}</li> <li>Accepts New Patients? ${data[i].practices[0].accepts_new_patients}</li></li>`);
-  //       }
-  //     } else {
-  //       $('#headAilment').html(`<h4>Sorry, there are no doctors that specialize in ${input}</h4>`);
-  //     }
-  //   }, function(error) {
-  //     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-  //   });
-  // });
-
   $('#byConditionQueryShow').click(function() {
     let input = $('#condition').val();
     $('#condition').val("");
@@ -47,8 +9,6 @@ $(document).ready(function() {
     let promise = doctorQuery.byConditionQuery(input);
     promise.then(function(response) {
       let body = JSON.parse(response);
-      console.log(body);
-
       let data = body.data;
       if (data.length > 0) {
         $('#headConditionQuery').html(`<h4>Here are some doctors that can help with ${input}</h4>`);
