b99f9cf5ca98552661f38d0ff4ff445172e095df
kaya <jepsonkaya@gmail.com>
2019-06-07 15:48:57 -0700

    add almost functional by specialty drop down menu

--- src/main.js
+++ src/main.js
@@ -60,7 +60,8 @@ $(document).ready(function() {
   });
 
   $('#byCity').click(function() {
-    $('.showDoctorsName').empty();
+    $('.byCityShow').empty();
+    $('#toggleCity').show();
     let input = $('#allDoc').val();
     $('#allDoc').val("");
     console.log(input);
@@ -83,26 +84,54 @@ $(document).ready(function() {
     });
   });
 
+  $('#toggleCity').on('click', function() {
+    $('.byCityShow').toggle();
+    $('#headByCity').toggle();
+  });
+
+
+  $('.showDoctorsName').empty();
+  let input = $('#allDoc').val();
+  $('#allDoc').val("");
+  let doctorQuery = new DoctorQuery();
+  let promise = doctorQuery.allSpecialties();
+  promise.then(function(response) {
+    let body = JSON.parse(response);
+    let data = body.data;
+    console.log("here", data);
+    console.log(data[0].name);
+    let dropdown = $('#specialty-dropdown');
+    dropdown.empty();
+    dropdown.append('<option selected="true" disabled>Choose a Specialty</option>');
+    dropdown.prop('selectedIndex', 0);
+    for (let i=0; i < data.length; i++){
+      dropdown.append($('<option></option>').attr('value', data[i].name).text(data[i].name))
+    }
+  });
+
   $('#bySpecialty').click(function() {
     $('.showDoctorsName').empty();
-    let input = $('#allDoc').val();
-    $('#allDoc').val("");
-    console.log(input);
+    let input = $('#specialty-dropdown').val().toLowerCase();
+    $('#specialty-dropdown').val("");
+    let splitInput = input.replace(" ", "%20");
+    console.log("input", input);
     let doctorQuery = new DoctorQuery();
-    let promise = doctorQuery.allSpecialties();
+    let promise = doctorQuery.bySpecialty(splitInput);
     promise.then(function(response) {
       let body = JSON.parse(response);
       let data = body.data;
-      console.log(data);
-      console.log(data);
-      let dropdown = $('#specialty-dropdown');
-      dropdown.empty();
-      dropdown.append('<option selected="true" disabled>Choose a Specialty</option>');
-      dropdown.prop('selectedIndex', 0);
-      for (let i=0; i < data.length; i++){
-        dropdown.append($('<option></option>').attr('value', data.name).text(data.name))
+      console.log("data", data);
+      if ((data).length > 0){
+        $('#headSpecialtyQuery').html(`<h4>Here are some doctors that specialize in ${input}</h4>`);
+        for (let i=0; i < data.length; i++){
+          $('.showSpecialtyQuery').append(`<li>${data[i].profile.first_name} ${data[i].profile.last_name}<ul><li>${data[i].practices[0].visit_address.street} ${data[i].practices[0].visit_address.city} ${data[i].practices[0].visit_address.state}</li> <li>${data[i].practices[0].phones[0].number}</li> <li>Accepts New Patients? ${data[i].practices[0].accepts_new_patients}</li></li>`);
+        }
+      } else {
+        $('#headSpecialty').html(`<h4>Sorry, there are no doctors that specialize in ${input}</h4>`);
       }
-    })
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
   });
 
 
